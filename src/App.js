a2d647a6c4e4bc2a1a514099c528d33920b6e88b
TiparpronS <96738451+SukanyaT2022@users.noreply.github.com>
2024-03-18 07:11:38 -0400

    add

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
+import Navbar from './Component/Navbar';
 import Home from './Home';
 
 function App() {
   return (
     <div className="App">
+         <Navbar/>
     <Home/>
 
     </div>
