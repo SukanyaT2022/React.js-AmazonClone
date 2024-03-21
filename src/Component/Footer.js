f1dba3911c831a2b6b4f1429b8f0bba967d0f687
TiparpronS <96738451+SukanyaT2022@users.noreply.github.com>
2024-03-21 07:10:38 -0400

    add

--- src/Component/Footer.js
+++ src/Component/Footer.js
@@ -1,11 +1,11 @@
 import React from 'react'
-
+import logo from '../Image/aLogo-bgremove.png'
 const Footer = () => {
   return (
     <div>
 
-<div className='mainBox bg-[#1e465c] grid lg:grid-cols-4  md:grid-cols-2  grid-cols-1 gap-4'>
-<div className='bg-green-200'>
+<div className='mainBox bg-[#1e465c] grid lg:grid-cols-4  md:grid-cols-2  grid-cols-2 gap-4 lg:p-10 lg:pl-28 md:py-5 md:pl-40 py-5 px-10 text-white'>
+<div className=''>
   <h3>Get to Know Us</h3>
 <ul>
 <li><a href=''>Careers</a></li>
@@ -16,7 +16,7 @@ const Footer = () => {
 
 </div>
 
-<div className='bg-red-200'>
+<div className=''>
 <h3 className='font-bold text-xl'>Get to Know Us</h3>
 <ul>
 <li><a href=''>Careers</a></li>
@@ -26,7 +26,7 @@ const Footer = () => {
 </ul>
 </div>
 
-<div className='bg-orange-800'>
+<div className=''>
 <h3>Get to Know Us</h3>
 <ul>
 <li><a href=''>Careers</a></li>
@@ -35,7 +35,7 @@ const Footer = () => {
 <li><a href=''>Press Center</a></li>
 </ul>
 </div>
-<div className='bg-yellow-200'>
+<div className=''>
 <h3>Get to Know Us</h3>
 <ul>
 <li><a href=''>Careers</a></li>
@@ -43,10 +43,25 @@ const Footer = () => {
 <li><a href=''>Investor Relations</a></li>
 <li><a href=''>Press Center</a></li>
 </ul>
+</div>
+</div>
+{/* end mainbox */}
+
+
+{/* below start extra navbar for right reserve */}
+<hr className=' bg-gray-200'/>
+<div className='bg-[#1e465c]'>
+<div className='logo'>
+<img src={logo}/>
+</div>
+<div className='condition'>
+<a href=''>Condition of Use</a>
+<a href=''>Privacy Notice</a>
+<a href=''>Your Ads Privacy Choices</a>
+<p>&copy;2024, EasyLife.com, Inc.</p>
 </div>
 </div>
 
-      
     </div>
   )
 }
--- src/Component/Navbar.js
+++ src/Component/Navbar.js
@@ -1,6 +1,12 @@
 import 'flowbite';
+import {useState} from "react";
 
 const Navbar = () => {
+  const [sidebar,setSidebar] = useState("hidden")
+
+  const openSidebar = ()=>{
+    setSidebar("")
+  }
   return (
     <div>
       <nav className="bg-black text-white border-gray-200 dark:bg-gray-900 fixed top-0 w-full ">
@@ -25,7 +31,7 @@ const Navbar = () => {
               type="button"
               data-collapse-toggle="navbar-search"
               aria-controls="navbar-search"
-              aria-expanded="false"
+              aria-expanded={sidebar}
               className="md:hidden text-gray-500 dark:text-gray-400 hover:bg-gray-100 dark:hover:bg-gray-700 focus:outline-none focus:ring-4 focus:ring-gray-200 dark:focus:ring-gray-700 rounded-lg text-sm p-2.5 me-1"
             >
               <svg
@@ -58,6 +64,7 @@ const Navbar = () => {
     </div> */}
             <button
               data-collapse-toggle="navbar-search"
+              onClick={openSidebar}
               type="button"
               className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
               aria-controls="navbar-search"
@@ -84,7 +91,7 @@ const Navbar = () => {
 
           {/* below div control the whole ul li and the search box */}
           <div
-            className="items-center justify-between hidden w-full md:flex md:w-[70%] flex-row-reverse  bg-green-400"
+            className={`items-center justify-between ${sidebar} w-full md:flex md:w-[70%] flex-row-reverse  bg-green-400`}
             id="navbar-search"
           >
             {/* searchbox on phone---- size-show only for phone-bigger devices it hide */}
