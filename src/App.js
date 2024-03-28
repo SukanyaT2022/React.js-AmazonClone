07a63b89623a2aaf6fcb47b3dbf83333dbe07f28
TiparpronS <96738451+SukanyaT2022@users.noreply.github.com>
2024-03-28 14:32:51 -0400

    add

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
-import Navbar from './Component/Navbar';
-import Test from './Component/Test';
-
 import './styles/App.css';
 import Home from './Pages/Home';
-
 import Order from './Pages/Order';
 import Cart from './Pages/Cart';
 import Account from './Pages/Account';
 // import { BrowserRouter as Router,Routes,Route } from 'react-router-dom';
 import { Routes, Route } from "react-router-dom"
 import Footer from './Component/Footer';
+import Navbar2 from './Component/Navbar2';
 
 
 
 function App() {
   return (
     <div className="App">
-{/* <Navbar/> */}
-<Test/>
+<Navbar2/>
+
         <Routes>
               <Route path="/" element={<Home/>}/>
               <Route path="/home" element={<Home/>}/>
