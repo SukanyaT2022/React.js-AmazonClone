5f855e16c69c092d2b4fe92a258f26333e609483
TiparpronS <96738451+SukanyaT2022@users.noreply.github.com>
2024-05-30 09:05:02 -0400

    add

--- src/App.js
+++ src/App.js
@@ -11,12 +11,15 @@ import ProductDetail from './Pages/ProductDetail';
 import AllProducts from './Pages/AllProducts';
 import SliderProductHome from './Component/SliderProductHome';
 import {useState} from "react";
+import NotFound from './Component/NotFound';
 
 function App() {
   const [data,setData] = useState([])
+  const [flag,setFlag] = useState(false)
   
   const dataHandler = (val)=>{
     setData(val);
+    setFlag(true)
   }
 
 
@@ -24,10 +27,11 @@ function App() {
     <div className="App">
         <Navbar2 onDataHandler = {dataHandler}/>
         {/* Conditional rendering */}
-        {data.length !==0 &&  <AllProducts data={data}/>}
+        {data.length !==0?  <AllProducts data={data}/>: flag && <NotFound/>}
         <Routes>
               <Route path="/" element={data.length === 0 && <Home/>}/>
               <Route path="/home" element={<Home/>}/>
+              <Route path="/notfound" element={<NotFound/>}/>
               <Route path="/order" element={<Order/>}/>
               <Route path="/cart" element={<Cart/>}/>
               <Route path="/account" element={<Account/>}/>
--- src/Component/Navbar2.jsx
+++ src/Component/Navbar2.jsx
@@ -67,7 +67,7 @@ const Navbar2 = ({onDataHandler}) => {
               class="md:hidden text-gray-500 dark:text-gray-400 hover:bg-gray-100 dark:hover:bg-gray-700 focus:outline-none focus:ring-4 focus:ring-gray-200 dark:focus:ring-gray-700 rounded-lg text-sm p-2.5 me-1"
             >
               <svg
-                className="w-5 h-5 bg-red-400"
+                className="w-5 h-5"
                 aria-hidden="true"
                 xmlns="http://www.w3.org/2000/svg"
                 fill="none"
@@ -87,22 +87,7 @@ const Navbar2 = ({onDataHandler}) => {
             <div class="relative hidden md:block w-full">
               <div class="absolute inset-y-0 end-0 pr-3 flex items-center ps-3 pointer-events-none" 
               onClick={searchHandler}>
-                {/* icon search */}
-                {/* <svg
-                  class="w-4 h-4 text-black"
-                  aria-hidden="true"
-                  xmlns="http://www.w3.org/2000/svg"
-                  fill="none"
-                  viewBox="0 0 20 20"
-                >
-                  <path
-                    stroke="currentColor"
-                    stroke-linecap="round"
-                    stroke-linejoin="round"
-                    stroke-width="2"
-                    d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z"
-                  />
-                </svg> */}
+               
                 <span class="sr-only">Search icon</span>
               </div>
               <input
@@ -146,32 +131,16 @@ const Navbar2 = ({onDataHandler}) => {
           >
             {/* logo and search in the same box */}
             <div class="relative mt-3 md:hidden">
-              <div class="absolute inset-y-0 start-0 flex items-center ps-3 pointer-events-none" 
-              onClick={searchHandler}>
-                <svg
-                  class="w-4 h-4 text-gray-500 dark:text-gray-400"
-                  aria-hidden="true"
-                  xmlns="http://www.w3.org/2000/svg"
-                  fill="none"
-                  viewBox="0 0 20 20"
-                >
-                  <path
-                    stroke="currentColor"
-                    stroke-linecap="round"
-                    stroke-linejoin="round"
-                    stroke-width="2"
-                    d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z"
-                  />
-                </svg>
-              </div>
- <input
+            <input
 
                 type="text"
                 id="search-navbar"
-                className="block w-full p-2 ps-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 md:hidden"
+                className="block w-full p ps-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 md:hidden"
                 placeholder="Search..."
-                onChange={searchHandler}
+                onChange={(e)=>setSearchItem(e.target.value)}
               />
+              <button onClick={searchHandler}><IoSearch  className='text-2xl absolute top-[7px] right-2  '/></button>
+
 
             </div>
             <ul class="flex flex-col p-4 md:p-0 mt-4 font-medium border border-gray-100 rounded-lg bg-gray-50 md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
